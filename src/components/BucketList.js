e2ecfa3aefe4c4274b831dfe8bc84fc8e35bc310
Leslie <rleslie1015@gmail.com>
2019-06-29 15:12:09 -0500

    paragraph

--- src/components/BucketList.js
+++ src/components/BucketList.js
@@ -4,6 +4,7 @@ const BucketList = props => {
     return(
         <div className="bucket-list">
             <h1>This is the Bucket List Component.</h1>
+            <p>this is a paragraph</p>
         </div>
     );
 }
